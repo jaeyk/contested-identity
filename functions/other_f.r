9d3c1afb2fec284399d2742014744c9e0505b100
Jae Yeon Kim <jaeyeonkim@berkeley.edu>
2020-09-05 12:28:59 -0700

    added multivariate regression analyses

--- functions/other_f.r
+++ functions/other_f.r
@@ -28,3 +28,48 @@ replace_partyid <- function(data){
         data 
         
 }
+
+analyze_multi_ate <- function(data){
+        
+treat <- bind_cols(data$par.treat %>% tidy(), data$se.treat %>% tidy())
+
+treat <- treat[,-3]
+
+colnames(treat) <- c("Name", "Estimate", "SE")
+
+control <- bind_cols(data$par.control %>% tidy(), data$se.control %>% tidy())
+
+control <- control[,-3]
+
+colnames(control) <- c("Name", "Estimate", "SE")
+
+result <- bind_rows(mutate(treat, Assignment = "Treatment"),
+                    mutate(control, Assignment = "Control"))
+result 
+}
+
+plot_multi_ate <- function(result){
+
+result %>%
+        filter(Name != "(Intercept)") %>%
+        ggplot(aes(fct_reorder(Name, Estimate), Estimate, ymax = Estimate + SE, ymin = Estimate - SE)) +
+        geom_pointrange() +
+        facet_wrap(~Assignment) +
+        geom_hline(yintercept = c(0), linetype = "dashed") +
+        labs(x = "") +
+        ylim(c(-4,4)) +
+        coord_flip()
+        
+}
+
+bind_visualize <- function(df1, df2){
+bind_rows(mutate(tidy(df1, conf.int = TRUE), category = "External motivation"),
+          mutate(tidy(df2, conf.int = TRUE), category = "Internal motivation")) %>%
+        filter(term != "(Intercept)") %>%
+        ggplot(aes(x = term y = estimate, ymin = conf.low, ymax = conf.high)) +
+        geom_pointrange() +
+        coord_flip() +
+        labs(y = "Estimate",
+             x ="")+
+        facet_wrap(~category) +
+        geom_hline(yintercept = c(0), linetype = "dashed")}
